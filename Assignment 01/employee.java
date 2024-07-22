54cb12b989f6eb66dfc8634f964bc1203a2ea023
Ganpat <coolganpat41@gmail.com>
2024-07-22 22:08:43 +0530

    set a program 3 to reverse the employee's name

---
+++ Assignment 01/employee.java
@@ -0,0 +1,24 @@
+// Set A 3
+
+import java.util.Scanner;
+
+public class employee {
+    public static void main(String[] args) {
+        System.out.println("Enter the name of employee: ");
+        Scanner sc = new Scanner(System.in);
+        String employee = sc.nextLine();
+
+        System.out.println(reverse(employee));
+
+    }
+
+    public static String reverse(String string) {
+        int stringLength = string.length();
+        String reversedString = "";
+
+        for (int i = stringLength - 1; i >= 0; i--)
+            reversedString += string.charAt(i);
+
+        return reversedString;
+    }
+}
