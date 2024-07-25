d5f7f43b1c5d80e1dafb0befa45a95a72f74a9f3
Ganpat <coolganpat41@gmail.com>
2024-07-25 20:16:51 +0530

    set b program 4 to print a pattern

---
+++ Assignment 01/pattern.java
@@ -0,0 +1,17 @@
+// Set B 4
+
+// 5 
+// 4 5
+// 3 4 5
+// 2 3 4 5
+// 1 2 3 4 5
+
+public class pattern {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            for (int j = i; j <= 5; j++)
+                System.out.print(j + " ");
+            System.out.println();
+        }
+    }
+}
